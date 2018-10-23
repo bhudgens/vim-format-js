e3d2548fdeebf54fb146b266439fdc6bf43137f4
Benjamin Hudgens <benjamin@benjamindavid.com>
2018-10-23 01:06:00 -0500

    Support a jsbeautifyrc for options

--- js/payload.js
+++ js/payload.js
@@ -4,6 +4,12 @@ var args = process.argv.splice(2);
 var js_beautify = require(args[0]).js_beautify;
 var options = args[1] || '{}';
 var options = JSON.parse(args[1] || '{}');
+try {
+  var homeDirOptions = process.env.HOME + '/.jsbeautifyrc';
+  options = JSON.parse(fs.readFileSync(homeDirOptions));
+} catch (e) {
+  /* Fall back to default behavior */
+}
 var input = fs.readFileSync('/dev/stdin').toString();
 var result = (js_beautify(input, options));
-util.puts(result);
+console.log(result);
