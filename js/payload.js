4d5040c2b411e840854c2b3db8889f752abd1a0d
Benjamin Hudgens <benjamin@benjamindavid.com>
2020-09-03 19:34:43 -0500

    I don't know which is correct

--- js/payload.js
+++ js/payload.js
@@ -1,7 +1,6 @@
-var util = require('util');
 var fs = require('fs');
 var args = process.argv.splice(2);
-var js_beautify = require('/usr/lib/node_modules/js-beautify/js/index.js').js_beautify;
+var js_beautify = require('/usr/lib/node_modules/js-beautify/js/index.js');
 var options = args[1] || '{}';
 var options = JSON.parse(args[1] || '{}');
 try {
