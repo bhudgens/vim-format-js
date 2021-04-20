eb81bd0b2f73442b6ff8a6b4cbb7e0b759472b51
Benjamin Hudgens <benjamin@benjamindavid.com>
2021-04-20 18:24:05 -0500

    Default for ubuntu

--- js/payload.js
+++ js/payload.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var args = process.argv.splice(2);
-var js_beautify = require('/usr/lib/node_modules/js-beautify/js/index.js');
+var js_beautify = require('/usr/local/lib/node_modules/js-beautify/js/index.js');
 var options = args[1] || '{}';
 var options = JSON.parse(args[1] || '{}');
 try {
